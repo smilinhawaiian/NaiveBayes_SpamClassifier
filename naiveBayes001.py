4fe7f2b7d2c114bd6bd98d7413e26c99a5067067
Sharice Mayer <sharicemayer@sharices-air.lan>
2019-02-23 20:16:08 -0800

    broken working on append to test/train arrays

--- naiveBayes001.py
+++ naiveBayes001.py
@@ -4,7 +4,9 @@
 # Classify Spambase data from UCI ML repository
 
 import numpy as np
-
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
 
 
 # 1. 
@@ -49,3 +51,73 @@ import numpy as np
 # compute precision
 # compute recall
 # create confusion matrix
+
+
+# python for main method:
+if __name__ == "__main__":
+    # Read in and create matrice of data
+    spambase_data = pd.read_csv("spambase/spambase_copy.csv", header=None).values
+
+    # split row instances into spam and not spam for training
+    # mixed for testing - keeping same proportions test/train sets
+
+    print("\n\n")
+    # counts for making sure data is split evenly
+    spam_count = -1
+    nospam_count = -1
+
+    # arrays for test and train data
+    train_data_spam = np.zeros(spambase_data.shape)
+    train_data_nospam = np.zeros(58)
+    test_data = np.zeros(58)
+    #print(train_data_spam)
+    #train_data_nospam = np.zeros(58)
+    #test_data = np.zeros(58)
+    print(train_data_spam)
+
+    #** add randomization of spambase data array here
+
+    for an_instance in spambase_data:
+        # check if an_instance is spam
+        # instance is spam
+        if(an_instance[-1] == 1):
+            spam_count = spam_count + 1
+            if(spam_count == 0): #first instance
+                train_data_spam = np.r_[train_data_spam, an_instance]
+            elif((spam_count%2) == 0):
+                # add to test_data array
+            else:
+                test_data = np.r_[test_data, an_instance]
+            #if(spam_count == 0): #first instance
+            #    train_data_spam = np.r_[train_data_spam, an_instance]
+            #print(an_instance[-1])
+        # instance not spam
+        else:
+            nospam_count = nospam_count + 1
+            if(nospam_count%2 ==0):
+                train_data_nospam = np.r_[train_data_nospam, an_instance]
+            else:
+                test_data = np.r_[test_data, an_instance]
+
+
+
+
+
+        #np.concatenate(an_instance,
+        #print(an_instance)
+
+#    train_data_spam = 
+#    train_data_nospam = 
+
+    
+#    print("\n\n")
+#    print(spambase_data)
+#    print("\n\n")
+
+    print(train_data_spam)
+#    transposed_spam = spambase_data.transpose()
+#    print(transposed_spam)
+    print("\n\n")
+#    test_labels = spambase_data[:,0]
+
+
